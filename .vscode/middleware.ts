b0f18bee122330812ec6f9346b4e9d8bb291ebb1
George <jorgeluismandujano76@gmail.com>
2026-05-13 19:27:21 -0700

    fix signout

---
+++ .vscode/middleware.ts
@@ -0,0 +1,15 @@
+import { auth } from "@/auth"
+import { NextResponse } from "next/request"
+
+export default auth((req) => {
+    const isLoggedIn = !!req.auth
+    const isOnLoginPage = req.nextUrl.pathname === "/"
+
+    if (!isLoggedIn && !isOnLoginPage) {
+        return NextResponse.redirect(new URL("/", req.url))
+    }
+})
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
